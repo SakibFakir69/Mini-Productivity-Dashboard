d93c35ad5a6c18dec571ba76ffefbc803f97b20f
Sakib Fakir <FakirSakib22232@gmail.com>
2025-05-26 21:00:16 +0600

    auth route added done

--- client/src/layouts/AuthLayoutes.jsx
+++ client/src/layouts/AuthLayoutes.jsx
@@ -6,6 +6,7 @@ import React from 'react'
 function AuthLayoutes() {
   return (
     <div>
+        
     
 
     </div>
--- client/src/routes/routes.jsx
+++ client/src/routes/routes.jsx
@@ -3,6 +3,9 @@
 import {BrowserRouter, createBrowserRouter} from 'react-router-dom'
 import MainLayoutes from '../layouts/MainLayoutes'
 import Home from '../page/home/Home';
+import AuthLayoutes from '../layouts/AuthLayoutes';
+import Signup from '../auth/Signup';
+import Signin from '../auth/Signin';
 
 
 const Router = createBrowserRouter([
@@ -17,6 +20,23 @@ const Router = createBrowserRouter([
             }
         ]
         
+    },
+
+    {
+
+        path:'/auth',
+        element:<AuthLayoutes/>,
+        children:[
+            {
+                path:'/auth/siginin',
+                element:<Signin/>
+            },
+            {
+                path:'/auth/signup',
+                element:<Signup/>
+            }
+        ]
+
     }
 
 
