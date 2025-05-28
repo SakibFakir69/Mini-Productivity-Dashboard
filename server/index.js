9d25effd2eb7f3108355b89af5b7355bb9c96808
Sakib Fakir <FakirSakib22232@gmail.com>
2025-05-28 21:28:10 +0600

    server succesfully deploy

--- server/index.js
+++ server/index.js
@@ -150,46 +150,6 @@ app.post('/api/login-jwt', async (req, res) => {
 
 
 
-
-// app.post('/api/register', async (req, res) => {
-
-//     const user = req.body;
-//     const { name, email, password } = user;
-//     console.log(user);
-
-//     try {
-
-//         if (!name || !email || !password) {
-//             return res.status(400).json({ message: 'All fields are required' })
-//         }
-
-//         //    user check
-
-//         const exitUser = await register_user_schema.findOne({ email });
-
-//         if (exitUser) {
-//             return res.status(400).json({ message: 'User already exists with this email' })
-//         }
-
-//         const newUser = new register_user_schema({ name, email, password });
-//         await newUser.save();
-
-
-//         //    message
-//         res.status(201).json({ message: 'User registered successfully' })
-
-
-//     } catch (error) {
-//         console.log(error);
-//         res.status(500).json({ message: 'Server error. Try again later.' })
-//     }
-
-
-
-// })
-
-// user get 
-
 app.get('/api/alluser', async (req, res) => {
 
     try {
@@ -295,7 +255,7 @@ app.patch('/api/tasksedit/:id', async (req, res) => {
 
     const result = await tasksUser.findByIdAndUpdate(id, task);
     res.status(200).json(result);
-    res.send(result);
+
 
 
 })
